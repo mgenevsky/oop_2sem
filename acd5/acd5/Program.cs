07e9567950af144ebb82619c2cef2af8e1fd188d
Geferli <maksgenevsky@gmail.com>
2020-04-15 22:12:41 +0300

    Lab 5

---
+++ acd5/acd5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acd5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var A = new MyLine(1, 6, 1, 2); //(x1,y1,x2,y2);
+            Console.WriteLine("Длинна прямой A = " + A.Lenght());
+            var B = new MySection(1, 1,3,3); //(x1,y1,x2,y2);
+            var C = new MySection(2, 2,4,4); //(x1,y1,x2,y2);
+            Console.WriteLine("Угл между отрезками B и C = " + MySection.Angle(B, C)+" радиан ");
+            Console.WriteLine("Угл между отрезками B и осью Oy = " + MySection.AngleOy(B) + " радиан ");
+            Console.WriteLine("Отрезок C имеет такие координаты - " + C.Parameters());
+        }
+    }
+}
---
+++ acd5/acd5_c++/acd5_c++.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "MyLine.h"
+#include "MySection.h"
+#include <math.h>
+
+using namespace std;
+    
+int main()
+{
+    setlocale(LC_ALL, "");
+    MyLine A = MyLine(1, 6, 1, 2); //(x1,y1,x2,y2);
+    cout << "Длинна прямой A = " << A.Lenght() << endl;
+    MySection B = MySection(1, 1, 3, 3); //(x1,y1,x2,y2);
+    MySection C = MySection(2, 2, 4, 4); //(x1,y1,x2,y2);
+    cout << "Угл между отрезками B и C = " << MySection::Angle(B, C) << " радиан " << endl;
+    cout << "Угл между отрезками B и осью Oy = " << MySection::AngleOy(B) << " радиан " << endl;
+    cout << "Отрезок C имеет такие координаты - "; C.Parameters();
+}
